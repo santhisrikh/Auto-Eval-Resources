527cce617c543443a24c613ec5d0341c258c9229
santhisrikh <santhisri.kha@gmail.com>
2022-09-08 08:20:56 +0530

    added:config file

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,32 @@
+// const { defineConfig } = require("cypress");
+
+// module.exports = defineConfig({
+//   defaultCommandTimeout: 500,
+//   e2e: {
+//     setupNodeEvents(on, config) {
+//       // https://github.com/bahmutov/cypress-failed-log
+//       require("cypress-failed-log/on")(on);
+//     },
+//   },
+// });
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  defaultCommandTimeout: 500,
+  video: false,
+  failOnStatusCode: false,
+  screenshotOnRunFailure: true,
   e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      // https://github.com/bahmutov/cypress-failed-log
-      require("cypress-failed-log/on")(on);
+      return require("./cypress/plugins/index.js")(on, config);
+    },
+  },
+
+  component: {
+    devServer: {
+      framework: "create-react-app",
+      bundler: "webpack",
     },
   },
 });
